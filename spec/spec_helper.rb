d238837b15dad0224b1fe19dc8c5ed751b5b2c36
Vishal25 <vishalchauthmal@gmail.com>
2017-06-16 15:18:51 +0530

    Add show action to the users controller

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -59,4 +59,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/v/3-0/docs
   config.infer_spec_type_from_file_location!
+
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
