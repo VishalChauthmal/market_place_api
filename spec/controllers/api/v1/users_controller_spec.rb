fcc186f53a99989df3ca6c5c08bd021761b14e16
Vishal25 <vishalchauthmal@gmail.com>
2017-06-21 18:34:33 +0530

    Refactor test headers for each request

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
 describe Api::V1::UsersController do
-	before(:each) do
-		request.headers['Accept'] = "application/vnd.marketplace.v1, #{Mime::JSON}"
-		request.headers['Content-Type'] = Mime::JSON.to_s
-	end
 
 	describe "GET #show" do
 		before(:each) do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -65,4 +65,9 @@ RSpec.configure do |config|
 
   # Including to test requests
   config.include Request::JsonHelpers, type: :controller
+  config.include Request::HeadersHelpers, type: :controller
+
+  config.before(:each, type: :controller) do
+    include_default_accept_headers
+  end
 end
