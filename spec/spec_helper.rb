8fee9d564d76a1aa67b8072ba3c3f5a09fd88267
Vishal25 <vishalchauthmal@gmail.com>
2017-06-22 15:04:26 +0530

    Add sessions controller create action

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -61,11 +61,10 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
 
-  config.include Devise::Test::ControllerHelpers, type: :controller
-
   # Including to test requests
   config.include Request::JsonHelpers, type: :controller
   config.include Request::HeadersHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
 
   config.before(:each, type: :controller) do
     include_default_accept_headers
