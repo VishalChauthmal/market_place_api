d94ae49fa8aeba610907cdace69be7875cdc584d
Vishal25 <vishalchauthmal@gmail.com>
2017-06-22 18:18:08 +0530

    Add authorization for the users controller

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -56,6 +56,7 @@ describe Api::V1::UsersController do
 		context "when is successfully updated" do
 			before(:each) do
 				@user = FactoryGirl.create :user
+				api_authorization_header @user.auth_token
 				patch :update, { id: @user.id, user: { email: "newmail@example.com" } }
 			end
 
@@ -70,6 +71,7 @@ describe Api::V1::UsersController do
 		context "when is not created" do
 			before(:each) do
 				@user = FactoryGirl.create :user
+				api_authorization_header @user.auth_token
 				patch :update, { id: @user.id, user: { email: "bademail.com" } }
 			end
 
@@ -90,6 +92,7 @@ describe Api::V1::UsersController do
 	describe "DELETE #destroy" do
 		before(:each) do
 			@user = FactoryGirl.create :user
+			api_authorization_header @user.auth_token
 			delete :destroy, { id: @user.id }
 		end
 
